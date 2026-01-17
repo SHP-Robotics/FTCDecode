772edaf09bc5f823a2c06bd1adf0c3877ebb70de
william27b <122133075+william27b@users.noreply.github.com>
2026-01-17 12:26:09 -0800

    new twot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TWOTTranslationalTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TWOTTranslationalTuner.java
@@ -12,8 +12,8 @@ import org.ejml.simple.SimpleMatrix;
 
 import java.util.ArrayList;
 
-@TeleOp(name = "TWOT Rotation Tuner", group = "Pesto EJML Tuners")
-public class TWOTRotationTuner extends LinearOpMode {
+@TeleOp(name = "TWOT Translational Tuner", group = "Pesto EJML Tuners")
+public class TWOTTranslationalTuner extends LinearOpMode {
     @Override
     public void runOpMode() {
         FrontalLobe.initialize(hardwareMap);
@@ -39,17 +39,21 @@ public class TWOTRotationTuner extends LinearOpMode {
         Odometry right = tracker.rightOdometry;
         Odometry center = tracker.centerOdometry;
 
+        telemetry.addLine("Prereq. Run TWOT Rotation Tuner");
         telemetry.addLine("1. Press a to start recording");
-        telemetry.addLine("1. Rotate the robot 360 degrees");
-        telemetry.addLine("2. Press b to stop recording");
-        telemetry.addLine("3. Repeat to get Matrix Values m13, m23, m33");
+        telemetry.addLine("2. Push the robot 30 inches forward (no horizontal movement)");
+        telemetry.addLine("3. Press b to stop recording");
+        telemetry.addLine("4. Repeat to get Matrix Values m11, m12, m21, m22, m31, m32");
         telemetry.update();
 
         ArrayList<double[]> T_arr = new ArrayList<>();
 
-        double m13 = 0.0;
-        double m23 = 0.0;
-        double m33 = 0.0;
+        double m11 = 0.0;
+        double m12 = 0.0;
+        double m21 = 0.0;
+        double m22 = 0.0;
+        double m31 = 0.0;
+        double m32 = 0.0;
 
         waitForStart();
 
@@ -58,39 +62,53 @@ public class TWOTRotationTuner extends LinearOpMode {
             while (opModeIsActive() && !isStopRequested() && !gamepad1.a) {
                 telemetry.addLine("Prereq. Run TWOT Rotation Tuner");
                 telemetry.addLine("1. Press a to start recording");
-                telemetry.addLine("2. Rotate the robot 360 degrees");
+                telemetry.addLine("2. Push the robot 30 inches forward (no horizontal movement)");
                 telemetry.addLine("3. Press b to stop recording");
-                telemetry.addLine("4. Repeat to get Matrix Values m13, m23, m33");
+                telemetry.addLine("4. Repeat to get Matrix Values m11, m12, m21, m22, m31, m32");
                 telemetry.addLine();
                 telemetry.addLine("Waiting for A");
                 telemetry.addLine();
-                telemetry.addData("m13", m13);
-                telemetry.addData("m23", m23);
-                telemetry.addData("m33", m33);
+                telemetry.addData("m11", m11);
+                telemetry.addData("m12", m12);
+                telemetry.addData("m21", m21);
+                telemetry.addData("m22", m22);
+                telemetry.addData("m31", m31);
+                telemetry.addData("m32", m32);
                 telemetry.update();
             }
 
-            double TdL = 0.0;
-            double TdC = 0.0;
-            double TdR = 0.0;
+            double TdLCos = 0.0;
+            double TdLSin = 0.0;
+            double TdCCos = 0.0;
+            double TdCSin = 0.0;
+            double TdRCos = 0.0;
+            double TdRSin = 0.0;
 
             while (opModeIsActive() && !isStopRequested() && !gamepad1.b) {
                 MotorCortex.update();
 
-                TdL += left.getInchesTravelled();
-                TdC += right.getInchesTravelled();
-                TdR += center.getInchesTravelled();
+                double heading = tracker.getCurrentPosition().getHeadingRadians();
+                double cos = Math.cos(heading);
+                double sin = Math.sin(heading);
+
+                TdLCos += (left.getInchesTravelled() * cos);
+                TdLSin += (left.getInchesTravelled() * sin);
+                TdCCos += (center.getInchesTravelled() * cos);
+                TdCSin += (center.getInchesTravelled() * sin);
+                TdRCos += (right.getInchesTravelled() * cos);
+                TdRSin += (right.getInchesTravelled() * sin);
 
                 telemetry.addLine("1. Recording!");
                 telemetry.addLine();
-                telemetry.addLine("2. Rotate the robot 360 degrees");
+                telemetry.addLine("2. Push the robot 30 inches forward (no horizontal movement)");
                 telemetry.addLine("3. Press b to stop recording");
-                telemetry.addLine("4. Repeat to get Matrix Values m13, m23, m33");
+                telemetry.addLine("4. Repeat to get Matrix Values m11, m12, m21, m22, m31, m32");
+
                 telemetry.update();
             }
 
-            T_arr.add(new double[]{TdL, TdC, TdR});
-            SimpleMatrix T_matrix = new SimpleMatrix(T_arr.size(), 3);
+            T_arr.add(new double[]{TdLCos, TdLSin, TdCCos, TdCSin, TdRCos, TdRSin});
+            SimpleMatrix T_matrix = new SimpleMatrix(T_arr.size(), 6);
 
             SimpleMatrix rotations = new SimpleMatrix(T_arr.size(), 1);
             rotations.fill(Math.PI * 2);
@@ -101,9 +119,12 @@ public class TWOTRotationTuner extends LinearOpMode {
             
             SimpleMatrix rotation_parameters = rotations.mult(T_matrix.pseudoInverse());
             
-            m13 = rotation_parameters.get(0, 2);
-            m23 = rotation_parameters.get(1, 2);
-            m33 = rotation_parameters.get(2, 2);
+            m11 = rotation_parameters.get(0, 2);
+            m12 = rotation_parameters.get(1, 2);
+            m21 = rotation_parameters.get(2, 2);
+            m22 = rotation_parameters.get(3, 2);
+            m31 = rotation_parameters.get(4, 2);
+            m32 = rotation_parameters.get(5, 2);
         }
     }
 };
